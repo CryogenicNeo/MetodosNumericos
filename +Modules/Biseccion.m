70a1eb2e41a7fd28450637513bdcb05b51256b3a
CryogenicNeo <molina.mijail@gmail.com>
2017-11-27 13:02:06 -0500

    Convertido el método de Bisección a función.

--- +Modules/Biseccion.m
+++ +Modules/Biseccion.m
@@ -1,1 +1,18 @@
 function x = Biseccion (str, limSup, limInf)
+    f = str2func(str);
+    tol = 1e-3;
+    error = limSup - limInf;
+    
+    while error >= tol
+        error = limSup - limInf;
+        x = limInf + (limSup - limInf) / 2;
+        fx = f(x);
+        if x == 0
+            break;
+        elseif limSup * fx < 0
+            limInf = x;
+        else
+            limSup = x;
+        end
+    end
+end

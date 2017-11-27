f7f06eee623db28bc8487ed43e504df59827f645
BagatirMarvinovich <molina.mijail@gmail.com>
2017-11-26 21:37:30 -0500

    Convertido el método de Simpson 3/8 compuesto a función.

--- +Modules/Simpson_38_Simple.m
+++ +Modules/Simpson_38_Simple.m
@@ -1,9 +1,9 @@
 function x = Simpson_38_Simple (str, limSup, limInf)
     f = str2func(str);
-    h= (limInf-limSup)/3;
+    h = (limInf - limSup) / 3;
 
-    disp('<< Integral Aproximada >>');
+    disp('<< Integral Aproximada >>')
     x =(3 * h / 8) * (f(limSup) + 3 * f((2 * limSup + limInf) / 3) + ...
         3 * f((2 + 2 * limInf) / 3));
-    fprintf('\n Respuesta \n %10.5f', x);
+    fprintf('\n Respuesta \n %10.5f', x)
 end

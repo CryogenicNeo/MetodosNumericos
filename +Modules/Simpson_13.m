7c98bb2fe5b96ad7e6b202a2cc61291d94eeb4d4
BagatirMarvinovich <molina.mijail@gmail.com>
2017-11-26 22:05:51 -0500

    Convertido el método de Simpson 1/3 a función.

--- +Modules/Simpson_13.m
+++ +Modules/Simpson_13.m
@@ -1,30 +1,27 @@
-%Metodo de Simpson 1/3%
-clear all; close all; clc
-fun=input('Ingresa la función f(x) entre comillas: ');
-f=inline(fun);
-n=1;
-while mod(n,2)~=0
-    n=input('Ingrese el número de subintervalos: ');
-    if mod(n,2)~=0
-        disp('El número de subintervalos debe ser par, pulse una tecla para continuar')
-        pause
+function Simpson_13 (str, limSup, limInf, inter)
+    f = str2func(str);
+
+    if nargin < 4
+        inter = 2;
     end
+
+    if mod(inter, 2) == 0
+        inter = inter + 1;
+    end
+
+    h = (limSup - limInf) / inter;
+
+    s1 = 0;
+    s2 = 0;
+
+    for i = 1 : 2 : (n-1)
+        s1 = s1 + feval(f, h * i + limInf);
+    end
+    for i = 2 : 2 : (n-2)
+        s2 = s2 + feval(f, h * i + limInf);
+    end
+
+    disp('<< Integral Aproximada >>')
+    x = (h / 3) * (feval(f, a) + 4 * s1 + 2 * s2 + feval(f, b));
+    fprintf('\n Respuesta \n %10.5f', x)
 end
-a=input('Ingrese el limite inferior de la integral: ');
-b=input('Ingrese el limite superior de la integral: ');
-
-h=(b-a)/n;
-
-sumai=0;
-sumap=0;
-
-for i=1:2:n-1
-    sumai=sumai+feval(f,h*i+a);
-end
-for i=2:2:n-2
-    sumap=sumap+feval(f,h*i+a);
-end
-
-int=(h/3)*(feval(f,a)+4*sumai+2*sumap+feval(f,b));
-
-disp(['El resultado de la integral es ' num2str(int)])
